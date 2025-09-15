3371057d18c937b34335ea1544ab95b38af6924f
Habib Ali Machpud <habib.machpud@gmail.com>
2025-09-15 15:26:27 +0700

    Add iterative invert binary tree method and tests
    Introduced invertTree2, an iterative method using a stack to invert a binary tree. Added three new test cases to validate the iterative approach alongside the existing recursive method.

--- leetcode/src/test/java/io/github/bluething/playground/codinginterview/java/leetcode/_226_InvertBinaryTreeTest.java
+++ leetcode/src/test/java/io/github/bluething/playground/codinginterview/java/leetcode/_226_InvertBinaryTreeTest.java
@@ -2,6 +2,8 @@ package io.github.bluething.playground.codinginterview.java.leetcode;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class _226_InvertBinaryTreeTest extends _01_ParentTreeTest {
@@ -62,7 +64,96 @@ class _226_InvertBinaryTreeTest extends _01_ParentTreeTest {
         assertTrue(areTreesEqual(result, null));
     }
 
+    @Test
+    void case04() {
+        // Input: root = [4,2,7,1,3,6,9]
+        // Output: [4,7,2,9,6,3,1]
+        TreeNode root = new TreeNode(4,
+                new TreeNode(2,
+                        new TreeNode(1),
+                        new TreeNode(3)
+                ),
+                new TreeNode(7,
+                        new TreeNode(6),
+                        new TreeNode(9)
+                )
+        );
+
+        TreeNode expected = new TreeNode(4,
+                new TreeNode(7,
+                        new TreeNode(9),
+                        new TreeNode(6)
+                ),
+                new TreeNode(2,
+                        new TreeNode(3),
+                        new TreeNode(1)
+                )
+        );
+
+        TreeNode result = invertTree2(root);
+        assertTrue(areTreesEqual(result, expected));
+    }
+
+    @Test
+    void case05() {
+        // Input: root = [2,1,3]
+        // Output: [2,3,1]
+        TreeNode root = new TreeNode(2,
+                new TreeNode(1),
+                new TreeNode(3)
+        );
+
+        TreeNode expected = new TreeNode(2,
+                new TreeNode(3),
+                new TreeNode(1)
+        );
+
+        TreeNode result = invertTree2(root);
+        assertTrue(areTreesEqual(result, expected));
+    }
+
+    @Test
+    void case06() {
+        // Input: root = []
+        // Output: []
+
+        TreeNode result = invertTree2(null);
+        assertTrue(areTreesEqual(result, null));
+    }
+
     private TreeNode invertTree(TreeNode root) {
-        return null;
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    private TreeNode invertTree2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Stack<TreeNode> nodes = new Stack<>();
+        nodes.push(root);
+        while (!nodes.isEmpty()) {
+            TreeNode current = nodes.pop();
+            if (current != null) {
+                TreeNode temp = current.left;
+                current.left = current.right;
+                current.right = temp;
+
+                nodes.push(current.left);
+                nodes.push(current.right);
+            }
+        }
+
+        return root;
     }
 }
